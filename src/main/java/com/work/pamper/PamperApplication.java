ac631e42e844476bf7b137eed10ff7f4aa99ffbb
ooyyh <laowan345@gmail.com>
2025-11-12 11:22:28 +0800

    项目初始化

---
+++ src/main/java/com/work/pamper/PamperApplication.java
@@ -0,0 +1,13 @@
+package com.work.pamper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PamperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PamperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/work/pamper/PamperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work.pamper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PamperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
