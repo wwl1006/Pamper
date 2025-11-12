5b78956d2032373ec13ae327671207a07c219bf4
ooyyh <laowan345@gmail.com>
2025-11-12 14:33:34 +0800

    fix: VueRouter的小问题

--- Pamper-Front/src/router/index.js
+++ Pamper-Front/src/router/index.js
@@ -7,16 +7,11 @@ const router = createRouter({
     {
       path: '/',
       name: 'index',
-      component: ()=> import('../views/IndexView.vue')
-    }
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
+      component: ()=> import('../views/IndexView.vue'),
+      children:[
+
+      ]
+    },
   ]
 })
 
--- README.md
+++ README.md
@@ -4,5 +4,3 @@
 - 前端：Vue 3 + Element Plus
 - 后端：Spring Boot + MyBatis Plus
 - 数据库：MySQL
-
-
