b706d2cf472910e95732f0792ca5b91216ea3ad6
ooyyh <laowan345@gmail.com>
2025-11-21 15:09:39 +0800

    #Fix 修复了登录页面和注册页面的Bug，封装了Request函数，以及用户类型选择

--- Pamper-Front/src/views/Register.vue
+++ Pamper-Front/src/views/Register.vue
@@ -3,12 +3,14 @@ import { ElMessage } from 'element-plus';
 // import { ca } from 'element-plus/es/locales.mjs';
 import { reactive } from 'vue';
 import { useRouter } from 'vue-router'
+import request from '../utils/request';
 
 const router = useRouter()
 const form = reactive({
   username: "",
   password: "",
-  repassword: ""
+  repassword: "",
+  user_type: 1 // 用户类型: 普通用户=1, 商户用户=2 (不支持管理员注册)
 })
 const doRegister = async () => {
   if (!form.username || !form.password) {
@@ -19,10 +21,11 @@ const doRegister = async () => {
     ElMessage.error("两次密码不一致，请重试！")
     return
   }
-  await registerUser({
+  await request.post('/account/register', {
     username: form.username,
     password: form.password,
-    repassword: form.repassword
+    // repassword: form.repassword,
+    user_type: form.user_type
   }).then(res => {
     if (res.code === 200) {
       ElMessage.success('注册成功，前往登录中...')
@@ -34,6 +37,21 @@ const doRegister = async () => {
     console.log("请求失败", err);
     ElMessage.error("服务器错误！")
   })
+  // await registerUser({
+  //   username: form.username,
+  //   password: form.password,
+  //   repassword: form.repassword
+  // }).then(res => {
+  //   if (res.code === 200) {
+  //     ElMessage.success('注册成功，前往登录中...')
+  //     setTimeout(login, 1000)
+  //   } else {
+  //     ElMessage.error(res.msg)
+  //   }
+  // }).catch(err => {
+  //   console.log("请求失败", err);
+  //   ElMessage.error("服务器错误！")
+  // })
 }
 const login = () => {
   router.push('/login')
@@ -54,6 +72,12 @@ const login = () => {
         </el-form-item>
         <el-form-item prop="repassword">
           <el-input class="login-input" v-model="form.repassword" placeholder="确认密码" prefix-icon="User"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-radio-group v-model="form.user_type">
+            <el-radio :label="1">普通用户</el-radio>
+            <el-radio :label="2">商户用户</el-radio>
+          </el-radio-group>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" class="login-btn" @click="doRegister">注册</el-button>
